49739b462cc7b2cb4e8246589dc4c5ae21b8d851
Kamakhi1994 <kpsamantaray2009@gmail.com>
2023-07-29 09:54:47 +0530

    commit

---
+++ frontend/src/app/components/sidepanel/sidepanel.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidepanel',
+  templateUrl: './sidepanel.component.html',
+  styleUrls: ['./sidepanel.component.css']
+})
+export class SidepanelComponent {
+
+   Left_sidepanel_Data={content1:"Vision and Mission",content2:"Role and Function",
+   content3:"Organisation and History",content4:""
+
+  }
+
+}
